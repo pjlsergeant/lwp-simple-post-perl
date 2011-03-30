165f622e2f63d548730b9aa16ebad3a586d7c569
Peter Sergeant <pete@clueball.com>
2011-03-30 06:33:55 +0100

    Final cleanup

--- eg/jargon_file.pl
+++ eg/jargon_file.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!perl
 
 # Very Contrived example. This module is used for doing boring
 # stuff internally by me, so it took a while to find an example
@@ -10,12 +10,11 @@ use warnings;
 use LWP::Simple::Post qw(post);
 use URI::Escape;
 
-my $term = $ARGV[0] || 'JAPH'; 
+my $term = $ARGV[0] || 'JAPH';
 
-my $result = post( 
+my $result = post(
 	'http://www.dict.org/bin/Dict',
 	'Form=Dict1&Strategy=*&Database=vera&Query=' . $term);
 
 my ( $definition ) = $result =~ m!<pre>([^<]{10,})</pre>!s;
 print $definition;
-	
--- t/01_sani.t
+++ t/01_sani.t
@@ -5,3 +5,6 @@ use warnings;
 
 use Test::More tests => 1;
 use_ok('LWP::Simple::Post');
+
+# My imagination is failing me a little...
+
